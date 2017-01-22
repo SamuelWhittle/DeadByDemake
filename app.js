5f4a08e5fb0035771c7bcfbfe726f4cf2aedf415
Offspec <picklenickle2@gmail.com>
2017-01-21 23:57:24 -0600

    Signed-off-by: Offspec <picklenickle2@gmail.com>

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var Player = function(id){
 		pressingDown:false,
 		maxSpd:10,
 	}
-	
+
 	self.updatePosition = function(){
 		if(self.pressingRight)// && !collision.inDeadZone(self.x+self.maxSpd, self.y))
 			self.x += self.maxSpd;
@@ -99,3 +99,37 @@ setInterval(function(){
 		socket.emit('newPositions',pack);
 	}
 },1000/25);
+
+var deadZones = [[20,20,50,50], [270,270,290,500]]
+
+function addDeadZone(x1, y1, x2, y2){
+  deadZones.push([x1,y1,x2,y2])
+}
+
+function cleanup(){
+  for(i = 0; i < deadZones.length; i++){
+    for(k = i; k < deadZones.length; k++){
+      if()
+    }
+  }
+}
+
+function removeDeadZone(x1,y1,x2,y2){
+  var temp = [x1,y1,x2,y2]
+  for(i = 0; i < deadZones.length; i++){
+    if(deadZones[i] == temp){
+      for(k = deadZones.length-1; k >= i; k--){
+        deadZones[k] = deadZones[k-1]
+      }
+      deadZones.pop()
+    }
+  }
+}
+
+function inDeadZone(x,y){
+    for(i = 0; i < deadZones.length; i++){
+      if((x >= deadZones[i][0] && x <= deadZones[i][2]) && ((y >= deadZones[i][1] && y <= deadZones[i][3])))
+        return true
+    }
+    return false
+}
--- js/collision.js
+++ js/collision.js
@@ -1,35 +1,37 @@
-module.exports = {
-  var deadZones = [[20,20,50,50], [270,270,290,500]]
+var deadZones = [[20,20,50,50], [270,270,290,500]]
 
-  function addDeadZone(x1, y1, x2, y2){
-    deadZones.push([x1,y1,x2,y2])
-  }
+function addDeadZone(x1, y1, x2, y2){
+  deadZones.push([x1,y1,x2,y2])
+}
 
-  function cleanup(){
-    for(i = 0; i < deadZones.length; i++){
-      for(k = i; k < deadZones.length; k++){
-        if()
+function cleanup(){
+  for(i = 0; i < deadZones.length; i++){
+    for(k = i; k < deadZones.length; k++){
+      if(deadZones[i] == deadZones[k]){
+        var temp = deadZones[i]
+        removeDeadZone(temp)
+        inDeadZone(temp)
       }
     }
   }
+}
 
-  function removeDeadZone(x1,y1,x2,y2){
-    var temp = [x1,y1,x2,y2]
-    for(i = 0; i < deadZones.length; i++){
-      if(deadZones[i] == temp){
-        for(k = deadZones.length-1; k >= i; k--){
-          deadZones[k] = deadZones[k-1]
-        }
-        deadZones.pop()
+function removeDeadZone(x1,y1,x2,y2){
+  var temp = [x1,y1,x2,y2]
+  for(i = 0; i < deadZones.length; i++){
+    if(deadZones[i] == temp){
+      for(k = deadZones.length-1; k >= i; k--){
+        deadZones[k] = deadZones[k-1]
       }
+      deadZones.pop()
     }
   }
+}
 
-  function inDeadZone(x,y){
-      for(i = 0; i < deadZones.length; i++){
-        if((x >= deadZones[i][0] && x <= deadZones[i][2]) && ((y >= deadZones[i][1] && y <= deadZones[i][3])))
-          return true
-      }
-      return false
-  }
+function inDeadZone(x,y){
+    for(i = 0; i < deadZones.length; i++){
+      if((x >= deadZones[i][0] && x <= deadZones[i][2]) && ((y >= deadZones[i][1] && y <= deadZones[i][3])))
+        return true
+    }
+    return false
 }
