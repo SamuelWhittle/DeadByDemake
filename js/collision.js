4cef70503922f6dc84b88050bb6f71f1bc1a6c7a
Offspec <picklenickle2@gmail.com>
2017-01-21 22:21:11 -0600

    Signed-off-by: Offspec <picklenickle2@gmail.com>

--- js/collision.js
+++ js/collision.js
@@ -4,5 +4,11 @@ function addDeadZone(x1, y1, x2, y2){
   deadZones.push([x1,y1,x2,y2])
 }
 
-function removeDeadZone(x1,y1,x2,y2)
+function removeDeadZone(x1,y1,x2,y2){
+  var temp = [x1,y1,x2,y2]
+  for(i = 0; i < deadZones.length; i++){
+    if(deadZones[i] == temp){
+      
+    }
+  }
 }
